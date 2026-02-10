47651b1edaabfbace9aa16c3bc345a29a748667a
Ayush Kumar Shah <103350464+AyushKuShah123@users.noreply.github.com>
2026-02-10 19:14:35 +0530

    sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,39 @@
-self.addEventListener("fetch", e => {});
+//self.addEventListener("fetch", e => {});
+
+const CACHE_NAME = "gifthouse-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/sw.js",
+  "/manifest.json"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if(key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
